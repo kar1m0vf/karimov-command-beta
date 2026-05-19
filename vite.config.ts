3fbb50ec1d34d950ebe1ce9dee6c11322f8db6c2
kar1m0vf <kamilkarimov16092006@gmail.com>
2026-05-19 21:23:34 +0400

    Изменить базовый путь в конфигурации Vite на '/karimov-command-beta/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/karimov-command-beta/',
   build: {
     target: 'es2020',
     cssMinify: true,
